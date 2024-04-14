d3431cdcf167ff57035d059c8ee4a568f06a9193
dyharlan <dyharlan45@gmail.com>
2024-04-14 21:55:21 +0800

    bla bla bla

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,21 +1,76 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use std::{
+  env, 
+  fs::OpenOptions,
+  io::{self, Write}, 
+  sync::{Arc, Mutex},
+  thread,
+  time,
+};
+use serde::Deserialize;
+use serde::Serialize;
+use futures::io::Window;
+use tauri::Window as TauriWindow;
 
+
+use libfprint_rs::{
+  FpPrint, 
+  FpDevice,
+};
+    
+use sqlx::{
+  MySqlPool,
+  Row,
+};
+use uuid::Uuid;
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
-
+#[derive(Serialize, Deserialize)]
+struct Employee {
+  emp_id: u64,
+  fname: String,
+  lname: String,
+}
 #[tauri::command]
-fn hello() -> String {
-  return String::from("hello");
+async fn enumerate_unenrolled_employees() -> String {
+  dotenvy::dotenv().unwrap();
+  let pool = MySqlPool::connect(&env::var("DATABASE_URL").unwrap()).await.unwrap(); 
+  let result = sqlx::query!("CALL enumerate_unenrolled_employees_json")
+  .fetch_all(&pool)
+  .await.unwrap();
+  
+   if result.is_empty() {
+      println!("No unenrolled employees found");
+      return String::from("{\"error\":\"No unenrolled employees found\"}");
+   }
+   let mut unenrolled: String = String::from("");
+   for (_,row) in result.iter().enumerate() {
+   	let json = row.get::<serde_json::Value, usize>(0);
+   	unenrolled = json.to_string();
+   }
+   return unenrolled;        
 }
 
+#[tauri::command]
+fn count(window: tauri::Window) {
+  thread::spawn(move || {
+    let mut times = 0;
+    let ten_millis = time::Duration::from_millis(1000);
+    while times <= 5 {
+      window.emit("num", times.to_string());
+      times+=1;
+      thread::sleep(ten_millis);
+    }
+  });
+  
+}
 fn main() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_sql::Builder::default().build())
-        .invoke_handler(tauri::generate_handler![greet,hello])
+        .invoke_handler(tauri::generate_handler![greet,enumerate_unenrolled_employees,count])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/enroll.js
+++ src/enroll.js
@@ -1,19 +1,34 @@
-//const { invoke } = window.__TAURI__.tauri;
-//import Database from "tauri-plugin-sql-api";
-import { invoke } from '@tauri-apps/api'
-// mysql
-//const db = await Database.load("mysql://root:toorenia@192.168.100.23/pyfi_db");
-
-//const result = await db.execute("call enumerate_unenrolled_employees()");
+const { invoke } = window.__TAURI__.tauri;
+const { listen } = window.__TAURI__.event;
 window.addEventListener("DOMContentLoaded", () => {
-  let queryString = window.location.search;
-  console.log(queryString);
-  let urlParams = new URLSearchParams(queryString);
-  console.log(urlParams.get('param'));
-  hello();
+  //let queryString = window.location.search;
+  //console.log(queryString);
+  //let urlParams = new URLSearchParams(queryString);
+  //console.log(urlParams.get('param'));
+  //hello();
+  enumerate_unenrolled_employees();
 });
-
-async function hello(){
-  let hello = await invoke("hello");
-  console.log(hello);
+window.unlisten = await listen("num", (ev) => {
+  document.querySelector("#tubol").textContent = ev.payload;
+});
+async function enumerate_unenrolled_employees(){
+  //await invoke('count');
+  let results = await invoke('enumerate_unenrolled_employees');
+  let results_json = JSON.parse(results);
+  for (var i = 0; i < results_json.length; i++){ //loop for each element
+  	var emp = results_json[i];
+  	if(emp.hasOwnProperty("error")){
+  		console.log("error: "+emp['error']);
+  		return;
+  	}
+  	if(emp.hasOwnProperty('emp_id')){
+  		console.log("emp_id: "+ emp['emp_id']);
+  	}
+  	if(emp.hasOwnProperty('fname')){
+  		console.log("first name : "+emp['fname']);
+  	}
+  	if(emp.hasOwnProperty('lname')){
+  		console.log("last name: "+emp['lname']);
+  	}			
+  }
 }
